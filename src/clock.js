c98faf3acf6df4cbdd4afd2b285304ed4b2afa1d
ElrohirGT <elrohirgt@gmail.com>
2022-04-27 15:22:39 -0600

    Basic conversion from clock class component to hooks

--- src/clock.js
+++ src/clock.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export class Clock extends React.Component {
-    TimerID = 0;
+export function Clock(props) {
+    const [date, setDate] = useState(new Date());
 
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
+    function tick() {
+        setDate(new Date());
     }
 
-    componentDidMount() {
-        this.TimerID = setInterval(()=>this.tick(), 1000);
-    }
+    useEffect(()=>{
+        let intervalId = setInterval(() => tick(), 1000);
+        return () => clearInterval(intervalId);
+    });
 
-    componentWillUnmount() {
-        clearInterval(this.TimerID);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello! </h1>
-                <h2>Current time: {this.state.date.toLocaleTimeString()}</h2>
-            </div>
-        );
-    }
-
-    tick() {
-        this.setState({ date: new Date() });
-    }
+    return (
+        <div>
+            <h1>Hello! </h1>
+            <h2>Current time: {date.toLocaleTimeString()}</h2>
+        </div>
+    );
 }
+
+// export class Clock extends React.Component {
+//     TimerID = 0;
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {date: new Date()};
+//     }
+
+//     componentDidMount() {
+//         this.TimerID = setInterval(()=>this.tick(), 1000);
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.TimerID);
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello! </h1>
+//                 <h2>Current time: {this.state.date.toLocaleTimeString()}</h2>
+//             </div>
+//         );
+//     }
+
+//     tick() {
+//         this.setState({ date: new Date() });
+//     }
+// }
