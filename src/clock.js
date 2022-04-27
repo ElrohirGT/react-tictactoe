7f7cc41e149b7dded5b96d2ee3a793fc26fef2b1
ElrohirGT <elrohirgt@gmail.com>
2022-04-27 16:08:13 -0600

    Deleted zombie code and improved performance in the effect

--- src/clock.js
+++ src/clock.js
@@ -7,10 +7,13 @@ export function Clock(props) {
         setDate(new Date());
     }
 
+    //Passing an empty array as the second argument
+    //will tell React this effect has no dependencies on properties of the component.
+    //Therefore it will be only cleaned once at the end.
     useEffect(()=>{
         let intervalId = setInterval(() => tick(), 1000);
         return () => clearInterval(intervalId);
-    });
+    }, []); 
 
     return (
         <div>
@@ -19,33 +22,3 @@ export function Clock(props) {
         </div>
     );
 }
-
-// export class Clock extends React.Component {
-//     TimerID = 0;
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {date: new Date()};
-//     }
-
-//     componentDidMount() {
-//         this.TimerID = setInterval(()=>this.tick(), 1000);
-//     }
-
-//     componentWillUnmount() {
-//         clearInterval(this.TimerID);
-//     }
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Hello! </h1>
-//                 <h2>Current time: {this.state.date.toLocaleTimeString()}</h2>
-//             </div>
-//         );
-//     }
-
-//     tick() {
-//         this.setState({ date: new Date() });
-//     }
-// }
